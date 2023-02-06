a2efabea84720d82f87ef6059fa3f8094a19e014
Nicolas Gardien <Nicolasgardien.developpeur@gmail.com>
2023-02-06 17:15:28 +0100

    Add firebase hosting

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import Home from "./Pages/Home";
 import Gallery from "./Pages/Gallery";
 import Prices from "./Pages/Prices";
 import Contact from "./Pages/Contact";
-import styled from "styled-components/macro";
 
 const App = () => {
   return (
-    <div>
-      <Wrapper>
-        <img src="./img/background-home.jpg" alt="backgroundImage" />
-        <h1 className="display-1">Charles Cantin - Photographe</h1>
-      </Wrapper>
+    <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/galerie" element={<Gallery />} />
@@ -24,26 +19,6 @@ const App = () => {
   );
 };
 
-const Wrapper = styled.div`
-  position: relative;
 
-  img {
-    height: 100vh;
-    width: 100%;
-    object-fit: cover;
-    filter: brightness(30%);
-  }
-  h1 {
-    position: absolute;
-    width: 100%;
-    text-align: center;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%);
-    color: white;
-    font-family: "DancingScript";
-    font-size: 50px;
-  }
-`;
 
 export default App;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import Navigation from '../Components/Navigation';
+import React from "react";
+import styled from "styled-components/macro";
+import Navigation from "../Components/Navigation";
 
 const Home = () => {
   return (
+    <Wrapper>
       <Navigation />
+      <h1>Charles Cantin - Photographe</h1>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled.div`
+  position: relative;
+  height: 100vh;
+
+  h1 {
+    z-index: 1;
+    position: absolute;
+    width: 100%;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%);
+    color: white;
+    font-family: "DancingScript";
+    font-size: 50px;
+  }
+`;
 
 export default Home;
